7e8d0b04f5774e83b7c726300990fd8aeca3b610
Zun-H-fighter <zunhyeok12@gmail.com>
2026-09-02 14:48:37 +0900

    Add IAM user and policy via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,40 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-# 연습용 S3 버킷
-resource "aws_s3_bucket" "practice" {
-    bucket = "zunfa-terraform-practice-2026"
+# IAM 사용자 (백업 업로드 전용)
+resource "aws_iam_user" "backup" {
+    name = "backup-uploader-tf"
 
     tags = {
         Project   = "hybrid-cloud-portfolio"
         ManagedBy = "Terraform"
     }
+}
+
+# IAM 정책 (특정 버킷 업로드 권한 only)
+resource "aws_iam_policy" "backup" {
+    name        = "s3-backup-policy-tf"
+    description = "백업 버킷에 대한 최소 권한(업로드/조회/삭제)"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid    = "ObjectActions"
+                Effect = "Allow"
+                Action = [
+                    "s3:PutObject",
+                    "s3:GetObject",
+                    "s3:DeleteObject"
+                ]
+                Resource = "arn:aws:s3:::zunfa-portfolio-backups-2026/*"
+            }
+        ]
+    })   
+}
+
+# 정책을 사용자에 연결
+resource "aws_iam_user_policy_attachment" "backup" {
+    user       = aws_iam_user.backup.name
+    policy_arn = aws_iam_policy.backup.arn
 }
